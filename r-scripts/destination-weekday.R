87251d7872bd89e047c8bdecd793a16841003331
Ankur Sinha <ankur218@yahoo.com>
2017-03-06 20:56:00 +0100

    Added source and destination analysis files

---
+++ r-scripts/destination-weekday.R
@@ -0,0 +1,14 @@
+library(MASS)
+file <- "mvg-dataset.csv"
+data <- read.csv(file, header=TRUE, sep=",")
+data <- data[(data$workingday == "Yes"),]
+destCount =  data$destination
+destCount.freq = table(destCount)
+png(filename = "destination-weekday.png", width=960, height=560, units="px")
+op0 = par()    
+op1 = op0$mar
+op1[2] = 25
+par(mar = op1)
+yy = barplot(sort(destCount.freq, increasing=TRUE), horiz=TRUE, xlab="Number Of Bikes", main="Incoming Bikes On Weekdays", xlim=c(0,10000), width=10, las=2, col="blue")
+text(y = yy, x = sort(destCount.freq, increasing=TRUE) + 750, label = sort(destCount.freq, increasing=TRUE), pos = 2, cex = 0.7, col = "red")
+dev.off()
---
+++ r-scripts/source-weekday.R
@@ -0,0 +1,14 @@
+library(MASS)
+file <- "mvg-dataset.csv"
+data <- read.csv(file, header=TRUE, sep=",")
+data <- data[(data$workingday == "Yes"),]
+sourceCount =  data$local_name
+sourceCount.freq = table(sourceCount)
+png(filename = "source-weekday.png", width=960, height=560, units="px")
+op0 = par()    
+op1 = op0$mar
+op1[2] = 25
+par(mar = op1)
+yy = barplot(sort(sourceCount.freq, increasing=TRUE), horiz=TRUE, xlab="Number Of Bikes", main="Outgoing Bikes On Weekdays", xlim=c(0,10000), width=10, las=2, col="blue")
+text(y = yy, x = sort(sourceCount.freq, increasing=TRUE) + 750, label = sort(sourceCount.freq, increasing=TRUE), pos = 2, cex = 0.7, col = "red")
+dev.off()
